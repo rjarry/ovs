1ea2e1bac38066e1fcc961bc079309920cd089ae
Robin Jarry <rjarry@redhat.com>
2023-01-11 08:51:43 +0100

    init tx_q pointer to null
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

--- lib/netdev-dpdk.c
+++ lib/netdev-dpdk.c
@@ -1313,6 +1313,7 @@ common_construct(struct netdev *netdev, dpdk_port_t port_no,
     dev->attached = false;
     dev->started = false;
     dev->reset_needed = false;
+    dev->tx_q = NULL;
 
     ovsrcu_init(&dev->qos_conf, NULL);
 
